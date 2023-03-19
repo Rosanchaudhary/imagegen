fae2b792cec6aabd450e3ba1140c86b252b52703
Rosanchaudhary <chaudharyroshan2020@gmail.com>
2023-03-19 20:20:11 +0545

    env

--- src/pages/screens/home-page.js
+++ src/pages/screens/home-page.js
@@ -1,11 +1,11 @@
-'use client'
+"use client";
 import React, { useState, useEffect } from "react";
 import { Loader, Card, FormField } from "../../components";
 
 const RenderCards = ({ data, title }) => {
   if (data?.length > 0) {
     return data.map((post) => <Card key={post._id} {...post} />);
-  } 
+  }
   return (
     <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
   );
@@ -57,6 +57,7 @@ const HomePage = () => {
   };
 
   useEffect(() => {
+    console.log(process.env.OPENAI_API_KEY);
     fetchPosts();
   }, []);
 
